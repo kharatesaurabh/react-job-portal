7c8fcd39550ae45f7a9f2e61c2bad95947f04413
saurabh <saurabhkharatesk1@gmail.com>
2025-06-26 17:17:17 +0530

    UI-changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import styles from'./Home.module.scss'
-import DrawerAppBar from '../components/AppBar'
-import Button from '@mui/material/Button'
+import React from 'react';
+import styles from './Home.module.scss';
+import DrawerAppBar from '../components/AppBar';
+import Button from '@mui/material/Button';
+import { Box, Typography } from '@mui/material';
 
 const Home = () => {
   return (
-    <div className={styles.mainConatiner}>
-        <DrawerAppBar/>
-        <div className={styles.buttonContainer}>
-
-        <Button variant="contained">View All Jobs</Button>
-        <Button variant="contained">Add Jobs</Button>
-
-        </div>
+    <div className={styles.mainContainer}>
+      <DrawerAppBar />
+      <Box className={styles.contentWrapper}>
+        <Typography variant="h4" className={styles.title}>
+          Welcome to Job Portal
+        </Typography>
+        <Box className={styles.buttonGroup}>
+          <Button variant="contained" color="primary" size="large">
+            View All Jobs
+          </Button>
+          <Button variant="outlined" color="secondary" size="large">
+            Add New Job
+          </Button>
+        </Box>
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
